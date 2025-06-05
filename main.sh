a5d269c89296aef260fbc2fc29f01cb0ecbd001c
= <=>
2025-06-05 14:01:00 +0530

    adding final option(half complete)

--- main.sh
+++ main.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 index=1;
-read -p "Enter File name(with extension): " file_name
-if [ -f "$file_name" ]; then
+read -p "Enter Extension:  " extension
+read -p "Enter File name(without extension): " file_name
+if [ -f "$file_name.$extension" ]; then
     echo "File exists."
+    echo "options:"
+    echo "1. run  2. skip  3. step backward  4. exit"
     echo "Contents of the file:"
     for line in $(cat "$file_name"); do
         echo "$index:  $line"
+        read -p "Choose an option: " option
+        if [ $option -eq 1 ]; then
+            cat "$file_name.$extension">>line
+            
+        fi
         let index=$index+1
     done
 else
