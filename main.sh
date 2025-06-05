afa03faed4fd4f8fab9799bd5fabea8e4dac77d8
= <=>
2025-06-05 13:37:03 +0530

    Checking File

--- main.sh
+++ main.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+read -p "Enter File name(with extension): " file_name
+if [ -f "$file_name" ]; then
+    echo "File exists."
+    echo "Contents of the file:"
+    for line in $(cat "$file_name"); do
+        echo "$line\n\n"
+    done
+else
+    echo "File does not exist."
+fi
