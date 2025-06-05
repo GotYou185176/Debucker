bb36cb5fcf892a51ce7ef0fa3994565fa37dd676
= <=>
2025-06-05 13:49:25 +0530

    adding index and making code loom good

--- main.sh
+++ main.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+index=1;
 read -p "Enter File name(with extension): " file_name
 if [ -f "$file_name" ]; then
     echo "File exists."
     echo "Contents of the file:"
     for line in $(cat "$file_name"); do
-        echo "$line\n\n"
+        echo "$index:  $line"
+        let index=$index+1
     done
 else
     echo "File does not exist."
